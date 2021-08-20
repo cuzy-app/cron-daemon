91a2666bdfc32e226cfc671cab06ed71889a41d9
funkycram <contact@marc.fun>
2021-08-20 11:24:37 +0200

    update

--- controllers/ExternalServiceController.php
+++ controllers/ExternalServiceController.php
@@ -8,6 +8,7 @@
 
 namespace humhub\modules\cronDaemon\controllers;
 
+use Yii;
 use yii\queue\db\Queue;
 use humhub\components\Controller;
 
@@ -18,6 +19,37 @@ class ExternalServiceController extends Controller
     {
         $queue = new Queue();
         $exitCode = $queue->run(0);
-        return ($exitCode === null) ? $this->returnSuccess('Queue executed') : $exitCode;
+        return ($exitCode === null) ? $this->returnSuccess('Queue executed') : $this->returnError(500, 'Queue not executed. Error code: '.$exitCode);
+    }
+
+
+
+    /**
+     * Generates error response
+     *
+     * @param int $statusCode
+     * @param string $message
+     * @param array $additional
+     * @return array
+     */
+    protected function returnError($statusCode = 400, $message = 'Invalid request', $additional = [])
+    {
+        Yii::$app->response->statusCode = $statusCode;
+        return array_merge(['code' => $statusCode, 'message' => $message], $additional);
+    }
+
+
+    /**
+     * Generates success response
+     *
+     * @param string $message
+     * @param int $statusCode
+     * @param array $additional
+     * @return array
+     */
+    protected function returnSuccess($message = 'Request successful', $statusCode = 200, $additional = [])
+    {
+        Yii::$app->response->statusCode = $statusCode;
+        return array_merge(['code' => $statusCode, 'message' => $message], $additional);
     }
 }
